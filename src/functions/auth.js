a63a9afef1c2acfd1c25af511461635b6160868d
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-07-07 20:53:11 +0530

    axios

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-
+const headers = {
+  'Content-Type': 'application/json',
+};
 export const signup = async (username, email, password) => {
-  return await axios.post(`${process.env.REACT_APP_API}/signup`, { username, email, password }, {});
+  return await axios.post(`${process.env.REACT_APP_API}/signup`, { username, email, password }, { headers });
 };
 export const signin = async (email, password) => {
-  return await axios.post(`${process.env.REACT_APP_API}/signin`, { email, password }, {});
+  return await axios.post(`${process.env.REACT_APP_API}/signin`, { email, password }, { headers });
 };
 export const logout = async (email, password) => {
   return await axios.get(`${process.env.REACT_APP_API}/logout`);
