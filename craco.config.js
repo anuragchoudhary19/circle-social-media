22f2aac6dc8be37e8456ab5092261eeb201b36c9
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-07-07 22:20:47 +0530

    craco

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,14 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
-        (plugin) => plugin instanceof MiniCssExtractPlugin
-      );
-      instanceOfMiniCssExtractPlugin.options.ignoreOrder = true;
+// module.exports = {
+//   webpack: {
+//     configure: (webpackConfig) => {
+//       const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
+//         (plugin) => plugin instanceof MiniCssExtractPlugin
+//       );
+//       instanceOfMiniCssExtractPlugin.options.ignoreOrder = true;
 
-      return webpackConfig;
-    },
-  },
-};
+//       return webpackConfig;
+//     },
+//   },
+// };
