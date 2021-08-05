d69d1243c3c43e9d2d0c829d011e5f7b6491f9f4
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-08-05 19:09:26 +0530

    mini-css-extract-plugin

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,27 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-};
+// module.exports = {
+//   webpack: {
+//     configure: (webpackConfig) => {
+//       const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
+//         (plugin) => plugin instanceof MiniCssExtractPlugin
+//       );
+//       instanceOfMiniCssExtractPlugin.options.ignoreOrder = true;
+
+//       return webpackConfig;
+//     },
+//   },
+// };
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// module.exports = {
+//   plugins: [new MiniCssExtractPlugin()],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//       },
+//     ],
+//   },
+// };
